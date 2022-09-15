dea05cc32bfb4ef5bd90760ec3da1c0ad842d33d
Andreas Schiffler <schiffler@roschi.eu>
2022-09-15 13:16:45 +0000

    setup dev finish

--- scripts/copy-bin.py
+++ scripts/copy-bin.py
@@ -5,10 +5,12 @@ print("Start")
 
 def post_program_action(source, target, env):
     print("Firmware bin-file was built!")
-    #os.rename(src, dst)
     src_bin = env.subst("${BUILD_DIR}/${PROGNAME}.bin")
     os.rename(src_bin,"esp-web-flash/firmware.bin")
-    print("Firmware bin-file was moved to Web-Flash-Tool root folder.")
+    print("---------------#########---------------")
+    print("Click on this url to flash the firmware: ")
+    url = os.system("gp url 3000");
+    print("---------------#########---------------")
    
 
 env.AddPostAction("$BUILD_DIR/${PROGNAME}.bin", post_program_action)
